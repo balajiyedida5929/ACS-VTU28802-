9fac8ca7584c117ea29e5cb6ffd93d6a8a3500cc
balajiyedida5929 <vtu28802@veltech.edu.in>
2026-08-27 09:56:48 +0530

    Create Move Zeroes.java

---
+++ Week-1/Session-5/Task-9/Move Zeroes.java
@@ -0,0 +1,26 @@
+// Program
+class Solution {
+    public void moveZeroes(int[] nums) {
+        int insertPos = 0;
+
+        for (int i = 0; i < nums.length; i++) {
+            if (nums[i] != 0) {
+                nums[insertPos] = nums[i];
+                insertPos++;
+            }
+        }
+
+        while (insertPos < nums.length) {
+            nums[insertPos] = 0;
+            insertPos++;
+        }
+    }
+}
+Accepted
+Runtime: 0 ms
+Case 1
+Case 2
+Input
+nums = [0,1,0,3,12]
+Output : [1,3,12,0,0]
+Expected : [1,3,12,0,0]
