22b1205f3c3d948bdeee178a619a36d8ae714b0e
balajiyedida5929 <vtu28802@veltech.edu.in>
2026-08-28 05:05:44 -0400

    Create Linked List Cycle II.java

---
+++ Week-2/Session-9/Task-17/Linked List Cycle II.java
@@ -0,0 +1,46 @@
+// Progrsm
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode(int x) : val(x), next(NULL) {}
+ * };
+ */
+class Solution {
+public:
+    ListNode *detectCycle(ListNode *head) {
+        ListNode* slow = head;
+        ListNode* fast = head;
+
+        while (fast != nullptr && fast->next != nullptr) {
+            slow = slow->next;
+            fast = fast->next->next;
+
+            if (slow == fast)
+                break;
+        }
+
+        if (fast == nullptr || fast->next == nullptr)
+            return nullptr;
+
+        ListNode* entry = head;
+
+        while (entry != slow) {
+            entry = entry->next;
+            slow = slow->next;
+        }
+
+        return entry;
+    }
+};
+Accepted
+Runtime: 3 ms
+Case 1
+Case 2
+Case 3
+Input
+head = [3,2,0,-4]
+pos = 1
+Output : tail connects to node index 1
+Expected : tail connects to node index 1
