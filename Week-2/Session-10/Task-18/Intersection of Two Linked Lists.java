098b3eeda75e28fc176ad2ba496ed53b37099e3d
balajiyedida5929 <vtu28802@veltech.edu.in>
2026-08-28 05:07:44 -0400

    Add intersection detection for two linked lists
    Implemented a solution to find the intersection of two linked lists.

---
+++ Week-2/Session-10/Task-18/Intersection of Two Linked Lists.java
@@ -0,0 +1,36 @@
+// Program
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode(int x) : val(x), next(NULL) {}
+ * };
+ */
+class Solution {
+public:
+    ListNode *getIntersectionNode(ListNode *headA, ListNode *headB) {
+        ListNode* a = headA;
+        ListNode* b = headB;
+
+        while (a != b) {
+            a = (a == nullptr) ? headB : a->next;
+            b = (b == nullptr) ? headA : b->next;
+        }
+
+        return a;
+    }
+};
+Accepted
+Runtime: 0 ms
+Case 1
+Case 2
+Case 3
+Input
+intersectVal = 8
+listA = [4,1,8,4,5]
+listB = [5,6,1,8,4,5]
+skipA = 2
+skipB = 3
+Output : Intersected at '8'
+Expected : Intersected at '8'
