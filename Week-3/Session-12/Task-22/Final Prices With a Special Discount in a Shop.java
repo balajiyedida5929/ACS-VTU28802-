01968a08226886fd9f52dca6c6988f253ae8c9f3
balajiyedida5929 <vtu28802@veltech.edu.in>
2026-08-29 09:30:21 +0530

    Add solution for final prices with discount problem

---
+++ Week-3/Session-12/Task-22/Final Prices With a Special Discount in a Shop.java
@@ -0,0 +1,28 @@
+// Program
+import java.util.*;
+
+class Solution {
+    public int[] finalPrices(int[] prices) {
+        Stack<Integer> stack = new Stack<>();
+
+        for (int i = 0; i < prices.length; i++) {
+            while (!stack.isEmpty() && prices[stack.peek()] >= prices[i]) {
+                int j = stack.pop();
+                prices[j] -= prices[i];
+            }
+            stack.push(i);
+        }
+
+        return prices;
+    }
+}
+
+Accepted
+Runtime: 0 ms
+Case 1
+Case 2
+Case 3
+Input
+prices = [8,4,6,2,3]
+Output : [4,2,4,2,3]
+Expected : [4,2,4,2,3]
