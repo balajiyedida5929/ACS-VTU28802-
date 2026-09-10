1113604f605979fe11206d6aa5a110c688e9b630
balajiyedida5929 <vtu28802@veltech.edu.in>
2026-09-10 10:00:28 +0530

    Create Binary Tree Level Order Traversal.java

---
+++ Week-4/Task-31/Binary Tree Level Order Traversal.java
@@ -0,0 +1,60 @@
+// Program
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+import java.util.*;
+
+class Solution {
+    public List<List<Integer>> levelOrder(TreeNode root) {
+        List<List<Integer>> result = new ArrayList<>();
+
+        if (root == null) {
+            return result;
+        }
+
+        Queue<TreeNode> queue = new LinkedList<>();
+        queue.offer(root);
+
+        while (!queue.isEmpty()) {
+            int size = queue.size();
+            List<Integer> level = new ArrayList<>();
+
+            for (int i = 0; i < size; i++) {
+                TreeNode node = queue.poll();
+                level.add(node.val);
+
+                if (node.left != null) {
+                    queue.offer(node.left);
+                }
+
+                if (node.right != null) {
+                    queue.offer(node.right);
+                }
+            }
+
+            result.add(level);
+        }
+         return result;
+    }
+}
+Accepted
+Runtime: 0 ms
+Case 1
+Case 2
+Case 3
+Input
+root = [3,9,20,null,null,15,7]
+Output : [[3],[9,20],[15,7]]
+Expected : [[3],[9,20],[15,7]]
