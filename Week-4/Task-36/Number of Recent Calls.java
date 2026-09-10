a4896f72a86cb423680131a270a4b302e49b0fce
balajiyedida5929 <vtu28802@veltech.edu.in>
2026-09-10 10:14:55 +0530

    Create Number of Recent Calls.java

---
+++ Week-4/Task-36/Number of Recent Calls.java
@@ -0,0 +1,32 @@
+// Program
+import java.util.*;
+
+class RecentCounter {
+    Queue<Integer> queue;
+
+    public RecentCounter() {
+        queue = new LinkedList<>();
+    }
+
+    public int ping(int t) {
+        queue.offer(t);
+
+        while (queue.peek() < t - 3000) {
+            queue.poll();
+        }
+
+        return queue.size();
+    }
+}
+/**
+ * Your RecentCounter object will be instantiated and called as such:
+ * RecentCounter obj = new RecentCounter();
+ * int param_1 = obj.ping(t);
+ */
+Accepted
+Runtime: 0 ms
+Case 1
+Input : ["RecentCounter","ping","ping","ping","ping"]
+        [[],[1],[100],[3001],[3002]]
+Output : [null,1,2,3,3]
+Expected : [null,1,2,3,3]
